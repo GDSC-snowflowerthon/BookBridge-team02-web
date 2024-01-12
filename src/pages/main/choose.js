b4b968c8477277ea26f3dcba7f66c1124e4179f0
기동언 <rechardkee@gmail.com>
2024-01-12 17:20:01 +0900

    choose-agencylogin연결

--- src/pages/main/choose.js
+++ src/pages/main/choose.js
@@ -1,39 +1,38 @@
-import React from 'react'
-import styled from '@emotion/styled'
-import backgroundImage from '../../assets/background/background.png'
-import donate3d from '../../assets/icon/donate3d.png'
-import PostBtn from '../../components/postbtn'
-import PostBtn1 from '../../components/postbtn1'
-import Header from '../../components/Layout/Header'
+import React from "react";
+import styled from "@emotion/styled";
+import backgroundImage from "../../assets/background/background.png";
+import donate3d from "../../assets/icon/donate3d.png";
+import PostBtn from "../../components/postbtn";
+import PostBtn1 from "../../components/postbtn1";
+import Header from "../../components/Layout/Header";
 const randomRange = (min, max) => {
-const rand = Math.random();
-return min + Math.floor(rand * (max - min + 1));
+  const rand = Math.random();
+  return min + Math.floor(rand * (max - min + 1));
 };
 
 const PostBg = styled.div`
-    background: url(${backgroundImage}) no-repeat center center fixed;
-    background-size: cover; /* 배경 이미지를 화면에 꽉 채우도록 설정 */
-    height: 876px; /* 화면 높이를 860px로 설정 */
-    width: 390px;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    cursor: pointer;
-    position: relative;
-    z-index: 0;
-    
+  background: url(${backgroundImage}) no-repeat center center fixed;
+  background-size: cover; /* 배경 이미지를 화면에 꽉 채우도록 설정 */
+  height: 876px; /* 화면 높이를 860px로 설정 */
+  width: 390px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  position: relative;
+  z-index: 0;
 `;
 
 const generateRandomStyles = () => {
-const randomX = Math.random() * 100;
-const randomOffset = randomRange(-100, 100);
-const randomXEnd = randomX + randomOffset;
-const randomXEndYoyo = randomX + randomOffset / 2;
-const randomYoyoTime = randomRange(30000, 80000) / 100000;
-const randomYoyoY = randomYoyoTime * 100;
-const randomScale = Math.random();
-const fallDuration = randomRange(10, 30);
-return {
+  const randomX = Math.random() * 100;
+  const randomOffset = randomRange(-100, 100);
+  const randomXEnd = randomX + randomOffset;
+  const randomXEndYoyo = randomX + randomOffset / 2;
+  const randomYoyoTime = randomRange(30000, 80000) / 100000;
+  const randomYoyoY = randomYoyoTime * 100;
+  const randomScale = Math.random();
+  const fallDuration = randomRange(10, 30);
+  return {
     randomX,
     randomXEnd,
     randomXEndYoyo,
@@ -41,84 +40,89 @@ return {
     randomYoyoY,
     randomScale,
     fallDuration,
+  };
 };
-};
-
 
 const generateSnowflakes = () => {
-const snowflakes = [];
+  const snowflakes = [];
 
-for (let i = 1; i <= 200; i++) {
+  for (let i = 1; i <= 200; i++) {
     const styles = generateRandomStyles();
 
     snowflakes.push({
-    id: i,
-    styles,
+      id: i,
+      styles,
     });
-}
+  }
 
-return snowflakes;
+  return snowflakes;
 };
 
 const Snow = styled.div`
-position: fixed;
-width: 10px;
-height: 10px;
-background: white;  // Change the background color to black
-pointer-events: none;
-border-radius: 50%;
+  position: fixed;
+  width: 10px;
+  height: 10px;
+  background: white; // Change the background color to black
+  pointer-events: none;
+  border-radius: 50%;
 
-${({ id, styles }) => `
+  ${({ id, styles }) => `
     &:nth-child(${id}) {
     opacity: ${Math.random()};
-    transform: translate(${styles.randomX}vw, -10px) scale(${styles.randomScale});
-    animation: fall-${id} ${styles.fallDuration}s -${Math.random() * 30}s linear infinite;
+    transform: translate(${styles.randomX}vw, -10px) scale(${
+    styles.randomScale
+  });
+    animation: fall-${id} ${styles.fallDuration}s -${
+    Math.random() * 30
+  }s linear infinite;
     }
 
     @keyframes fall-${id} {
     ${styles.randomYoyoTime * 100}% {
-        transform: translate(${styles.randomXEnd}vw, ${styles.randomYoyoY}vh) scale(${styles.randomScale});
+        transform: translate(${styles.randomXEnd}vw, ${
+    styles.randomYoyoY
+  }vh) scale(${styles.randomScale});
     }
     
     to {
-        transform: translate(${styles.randomXEndYoyo}vw, 100vh) scale(${styles.randomScale});
+        transform: translate(${styles.randomXEndYoyo}vw, 100vh) scale(${
+    styles.randomScale
+  });
     }
     }
 `}
 `;
 
-
-
 const BoxImg = styled.div`
-	position:fixed;
-	z-index:2;
-		position:absolute;
-		left:50%;
-		transform:translateX(-50%);
-		bottom:30%;
-	
+  position: fixed;
+  z-index: 2;
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+  bottom: 30%;
 `;
 const Hello = () => {
-const snowflakes = generateSnowflakes();
+  const snowflakes = generateSnowflakes();
 
-
-return (
-    <>        
-    <PostBg  >
-    {snowflakes.map((snowflake) => (
-        <Snow key={snowflake.id} id={snowflake.id} styles={snowflake.styles} />
-    ))}
+  return (
+    <>
+      <PostBg>
+        {snowflakes.map((snowflake) => (
+          <Snow
+            key={snowflake.id}
+            id={snowflake.id}
+            styles={snowflake.styles}
+          />
+        ))}
         <Header />
-        <PostBtn1  value='참고서 기증하기' type='button' to='/check'/>
+        <PostBtn1 value="참고서 기증하기" type="button" to="/check" />
         <BoxImg>
-            
-        <img src={donate3d}/>
+          <img src={donate3d} />
         </BoxImg>
-        <PostBtn value='등록하기' type='button' to='/post'/>
-    </PostBg>
+        <PostBtn value="등록하기" type="button" to="/reciever/agencylogin" />
+      </PostBg>
     </>
-
-);
+  );
 };
 
 export default Hello;
